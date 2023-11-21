cad507da23bcd98fb0508da18e1d18a68ba25aec
Rafael Caal <rafael.caal@gmail.com>
2023-11-20 22:45:57 -0600

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {NavController, AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+  cantidad : string;
+  resultado : number;
 
   constructor() {}
+
+  calcular(){
+    this.resultado = parseFloat(this.cantidad) / 8.0; 
+  }
 
 }
